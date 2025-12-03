e9358d4952b1cb8359c8227f781d0c4193472dd7
Vatinas <alexis.laouar@live.fr>
2025-12-03 14:42:37 +0100

    Added another hook taken from Quick Chat - Works with Stimm Supply in the Psykhanium, untested in live game

--- scripts/mods/ComeGetYourDrugs/ComeGetYourDrugs.lua
+++ scripts/mods/ComeGetYourDrugs/ComeGetYourDrugs.lua
@@ -79,5 +79,83 @@ local _place_unit_hook_function = function(self, action_settings, position, rota
     end
 end
 
---mod:hook_safe(CLASS.ActionPlacePickup, "_place_unit", _place_unit_hook_function)
-mod:hook_safe(CLASS.ActionPlaceDeployable, "_place_unit", _place_unit_hook_function)
+--mod:hook_safe(CLASS.ActionPlaceDeployable, "_place_unit", _place_unit_hook_function)
+
+
+
+
+------
+------
+
+local _init = function()
+    mod._owner_session_id = nil
+end
+
+mod.on_all_mods_loaded = function()
+    _init()
+end
+
+mod.on_game_state_changed = function(status, state_name)
+    if state_name == "StateLoading" and status == "enter" then
+        _init()
+    end
+end
+
+
+mod:hook_safe("Unit", "animation_event", function(unit, event)
+    if event == "drop" then
+        local player = Managers.player:player_by_unit(unit)
+
+        if player and player:is_human_controlled() then
+            mod._owner_session_id = player:session_id()
+        end
+    end
+end)
+
+mod:hook_safe("Unit", "flow_event", function(unit, event)
+    if event == "lua_deploy" and mod._owner_session_id then
+        local deployable_type = Unit.has_data(unit, "deployable_type") and Unit.get_data(unit, "deployable_type")
+
+        if not deployable_type == "medical_crate" then
+            -- This means the used item is not a medkit and not a stimm pack
+            return
+        end
+
+        local player = Managers.player:local_player(1)
+        local deploying_player = Managers.player:player_from_session_id(mod._owner_session_id)
+        local deploying_player_name = deploying_player._profile and deploying_player:name()
+
+        if not deploying_player or not player then
+            return
+        end
+
+        mod:echo("Player "..deploying_player_name.." placed deployable "..deployable_type)
+
+        if not deploying_player == player then
+            return
+        end
+
+        local deployable_position = Unit.world_position(unit, 1)
+
+        place_marker(deployable_position)
+
+        --local player_slot = player.slot and player:slot()
+        --local player_name = player._profile and player:name()
+        --local slot_color = mod:get("enable_slot_color") and player_slot and UISettings.player_slot_colors[player_slot]
+        --local suffix = is_local_player(player) and "self" or "others"
+        --local event_id = "auto_deployed_:s:_" .. suffix
+        --local message_type = "deploy_"
+
+        --[[
+        if Unit.has_data(unit, "pickup_type") then
+            event_id = string.gsub(event_id, ":s:", Unit.get_data(unit, "pickup_type"))
+            message_type = message_type .. "ammo"
+        else
+            event_id = string.gsub(event_id, ":s:", "medical_crate_deployable")
+            message_type = message_type .. "med"
+        end
+        --]]
+
+        mod._owner_session_id = nil
+    end
+end)
